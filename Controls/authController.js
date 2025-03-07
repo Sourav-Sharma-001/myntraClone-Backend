110cb79f2cc29e69cfa170810b9fe278ca6403eb
Sourav Sharma <sourav060606@gmail.com>
2025-03-07 18:26:04 +0530

    for  deployment

--- Controls/authController.js
+++ Controls/authController.js
@@ -1,5 +1,5 @@
 const userModel = require('../Models/users');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
